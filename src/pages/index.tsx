fcb27045f63402417f478c604eca4dc5457ab682
ren95 <71228030+ren95@users.noreply.github.com>
2022-01-23 14:35:23 +0100

    added fonts to public

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,24 @@ export default function Home() {
     <div className="flex flex-col min-h-screen w-full h-full">
       <Head>
         <title>muchoTodo</title>
+        <link
+          rel="preload"
+          href="../public/fonts/IntegralCF-Bold.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="../public/fonts/IntegralCF-Medium.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="../public/fonts/IntegralCF-Regular.otf"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
 
       <Layout />
